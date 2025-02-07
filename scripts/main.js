9b46c5bbc85f221d021d3f97f5b25b73fbfd851b
hazhdev <hazhbekar.zamaev@gmail.com>
2025-02-07 16:47:07 +0300

    Сделал стилизацию сайта и добавил функционал для удаления карточки

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+const page = document.querySelector('body');
+const cardobj = [
+   {
+      title: 'Frontend',
+      img: './assets/images/frontend.jpg',
+      subscribe: 'Мы ищем опытных Frontend разработчиков',
+      delete: './assets/icon/delete.png'
+   },
+   {
+      title: 'Backend',
+      img: './assets/images/backend.jpg',
+      subscribe: 'Мы ищем опытных Backend разработчиков',
+      delete: './assets/icon/delete.png'
+   }
+]
+
+const template = page.querySelector('.item__template').content
+const cardAdd = page.querySelector('.card__link')
+
+function clone(obj) {
+   const clonetemplate = template.cloneNode(true)
+
+   clonetemplate.querySelector('.card__title').textContent = obj.title
+   clonetemplate.querySelector('.card__img').src = obj.img
+   clonetemplate.querySelector('.card__description').textContent = obj.subscribe
+
+   removecard(clonetemplate)
+
+   cardAdd.appendChild(clonetemplate)
+}
+
+cardobj.forEach(item => {
+   clone(item)
+});
+
+
+function removecard(clone) {
+   const remove = clone.querySelector('.remove__curd')
+   const itemCard = page.querySelector('.itemCard')
+   remove.addEventListener('click', () => {
+      remove.parentElement.parentElement.remove();
+
+   })
+}
+
+
